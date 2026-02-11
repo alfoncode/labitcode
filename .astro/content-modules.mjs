b7c0629ae51f6824716bedb27123cd45b57b8260
Alfonso José García Bañón <alfoncode@Alfonsos-MacBook-Pro.local>
2026-02-11 23:59:37 +0100

    Upload configs

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/blog/ai-driven-development.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-driven-development.mdx&astroContentModuleFlag=true")],
-["src/content/blog/markdown-style-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.mdx&astroContentModuleFlag=true")],
-["src/content/projects/secureflow-scanner.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fsecureflow-scanner.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/building-labitcode.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbuilding-labitcode.mdx&astroContentModuleFlag=true")],
-["src/content/projects/labitcode-platform.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Flabitcode-platform.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/markdown-style-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.mdx&astroContentModuleFlag=true")],
+["src/content/projects/labitcode-platform.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Flabitcode-platform.mdx&astroContentModuleFlag=true")],
+["src/content/projects/secureflow-scanner.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fsecureflow-scanner.mdx&astroContentModuleFlag=true")]]);
 		
